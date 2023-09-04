e65291c348e465a21c8f6785089e77e733b8304d
Ruhani FashionFantasy <ruhanifashionfantasy@Ruhanis-MacBook-Air.local>
2023-09-04 11:03:09 +0530

    Task Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
-function App() {
+
+import React, { useState,useEffect } from 'react'
+import TextOverlay from './components/TextOverlay/TextOverlay';
+
+const App = () => {
+  const [imageUrl, setImageUrl] = useState('');
+  useEffect(()=>{
+    const fetchRandomImages= async ()=>{
+      try {
+        const response=await axios.get("https://api.unsplash.com/photos/random/?client_id=wuVH072bJSHESgb9apsPQnMNie2y3V3Irzs-bd38knM",{
+          headers:{
+            Authorization:"wuVH072bJSHESgb9apsPQnMNie2y3V3Irzs-bd38knM"
+          }
+        })
+        console.log(response.data.urls.regular);
+      
+        setImageUrl(response.data.urls.regular);
+      } catch (error) {
+        
+          console.error("Error in fetching data: " + error);
+        
+      }
+    }
+    if (!imageUrl) {
+      fetchRandomImages();
+    }
+  },[imageUrl])
+
+  const [textOverlays, setTextOverlays] = useState([]);
+  const [customText,setCustomText]=useState("");
+
+  const addTextOverlay=()=>{
+    if(customText){
+      setTextOverlays([
+        ...textOverlays,
+        {
+          text: customText,
+          position: { x: 100, y: 100 },
+        }
+      ]);
+      setCustomText("");
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div>
+      <img className='random-bg' src={imageUrl} alt='unsplash-random-image' />
     </div>
-  );
+
+    <div className='overlay'>
+      <input
+        className='input'
+        type='text'
+        value={customText}
+        onChange={(e) => setCustomText(e.target.value)}
+      />
+      <button onClick={addTextOverlay} className='btn'>Add Text</button>
+    </div>
+
+    {textOverlays.map((overlay, index) => (
+      <TextOverlay
+        key={index}
+        text={overlay.text}
+        position={overlay.position}
+        onDrag={(e, data) => {
+          // Update the position when dragged
+          const updatedOverlays = [...textOverlays];
+          updatedOverlays[index].position = { x: data.x, y: data.y };
+          setTextOverlays(updatedOverlays);
+        }}
+        onResize={(size) => {
+          // Handle resizing if needed
+          // You can access the width and height of the resized overlay via the 'size' parameter
+          console.log(`Resized to width: ${size.width}, height: ${size.height}`);
+        }}
+      />
+    ))}
+  </div>
+  )
 }
 
-export default App;
+export default App
+
+
+
---
+++ src/components/TextOverlay/TextOverlay.jsx
@@ -0,0 +1,64 @@
+import React, {useState} from 'react';
+import Draggable from "react-draggable";
+import { Resizable } from "react-resizable";
+
+
+const TextOverlay = ({text,position, onDrag, onResize}) => {
+   
+    const [editableText, setEditableText] = useState(text);
+    const [isEditing, setIsEditing] = useState(false);
+  
+    const handleDrag = (e, data) => {
+      // Handle your custom logic while dragging here
+      // For example, you can update the position of the text overlay
+      // based on the drag data
+    };
+  
+    const handleTextDoubleClick = () => {
+      setIsEditing(true);
+    };
+  
+    const handleTextBlur = () => {
+      setIsEditing(false);
+    };
+  
+    const handleTextChange = (e) => {
+      setEditableText(e.target.value);
+    };
+    const fontSize=3;
+  
+    return (
+        <Draggable defaultPosition={position} onDrag={handleDrag}>
+      <div
+        style={{
+          position: 'absolute',
+          top: `${position.y}px`,
+          left: `${position.x}px`,
+          border: '1px solid #000',
+          boxSizing: 'content-box',
+          fontSize: `${fontSize}rem`,
+        }}
+      >
+        {isEditing ? (
+          <input
+           style={{
+                
+               
+               
+                fontSize: '3rem', 
+              }}
+            type="text"
+            value={editableText}
+            onChange={handleTextChange}
+            onBlur={handleTextBlur}
+            autoFocus
+          />
+        ) : (
+          <div onDoubleClick={handleTextDoubleClick}>{editableText}</div>
+        )}
+      </div>
+    </Draggable>
+    );
+}
+
+export default TextOverlay
